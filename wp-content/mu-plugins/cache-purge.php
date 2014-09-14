017c7075d6d6d46de744e93e7749858e9a55e71e
Scott Taylor <scott.c.taylor@mac.com>
2014-09-14 15:33:07 -0400

    Update cache-purge.php

--- wp-content/mu-plugins/cache-purge.php
+++ wp-content/mu-plugins/cache-purge.php
@@ -16,24 +16,25 @@ class CachePurge {
 	}
 
 	public function transition_post_status( $new, $old, $post ) {
-        if ( 'publish' !== $old && 'publish' !== $new ) {
-            return;
+		if ( 'publish' !== $old && 'publish' !== $new ) {
+			return;
 		}
-        $post = get_post( $post );
-        $url = get_permalink( $post );
+		$post = get_post( $post );
+		$url = get_permalink( $post );
 
-        // Purge this URL
-        $this->purge( $url );
+		// Purge this URL
+		$this->purge( $url );
 
-        // Purge the front page
-        $this->purge( home_url( '/' ) );
-    }
+		// Purge the front page
+		$this->purge( home_url( '/' ) );
+	}
 
-    private function purge( $url ) {
-        wp_remote_get( $url, array(
+	private function purge( $url ) {
+		wp_remote_get( $url, array(
 			'timeout' => 0.01,
 			'blocking' => false,
 			'headers' => array( 'X-Nginx-Cache-Purge' => '1' )
 		) );
-    }
+	}
 }
+CachePurge::get_instance();

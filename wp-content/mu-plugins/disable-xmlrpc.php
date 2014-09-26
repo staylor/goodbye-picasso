ea40c975d4969724e213961a75eaf433ff0935bb
Scott Taylor <scott.c.taylor@mac.com>
2014-09-26 18:28:13 -0400

    Update disable-xmlrpc.php

--- wp-content/mu-plugins/disable-xmlrpc.php
+++ wp-content/mu-plugins/disable-xmlrpc.php
@@ -1,3 +1,13 @@
 <?php
 
-add_filter('xmlrpc_enabled', '__return_false');
+add_filter( 'xmlrpc_enabled', '__return_false' );
+
+function _remove_pingback_method( $methods ) {
+   unset( $methods['pingback.ping'] );
+   unset( $methods['pingback.extensions.getPingbacks'] );
+   return $methods;
+}
+add_filter( 'xmlrpc_methods', '_remove_pingback_method' );
+
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );

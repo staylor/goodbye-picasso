b6b49af5f90e36b51c95d10ae381e9afe930bcfc
Scott Taylor <scott.c.taylor@mac.com>
2015-10-26 16:27:38 -0400

    Fix

--- wp-content/plugins/gigpress/output/gigpress_shows.php
+++ wp-content/plugins/gigpress/output/gigpress_shows.php
@@ -16,8 +16,8 @@ function gigpress_archive($filter = null, $content = null) {
 function gigpress_shows($filter = null, $content = null) {
 
 	global $wpdb, $gpo;
-	$further_where = $limit = '';
-	
+	$further_where = $limit = $month = '';
+
 	extract(shortcode_atts(array(
 			'tour' => FALSE,
 			'artist' => FALSE,
@@ -35,9 +35,9 @@ function gigpress_shows($filter = null, $content = null) {
 			'month' => FALSE
 		), $filter)
 	);
-	
+
 	$total_artists = $wpdb->get_var("SELECT count(*) from " . GIGPRESS_ARTISTS);
-	
+
 	// Date conditionals and sorting based on scope
 	switch($scope) {
 		case 'upcoming':
@@ -57,30 +57,30 @@ function gigpress_shows($filter = null, $content = null) {
 			if(empty($sort)) $sort = 'desc';
 			break;
 	}
-	
+
 	// Artist, tour and venue filtering
 	if($artist) $further_where .= ' AND show_artist_id = ' . $wpdb->prepare('%d', $artist);
 	if($tour) $further_where .= ' AND show_tour_id = ' . $wpdb->prepare('%d', $tour);
 	if($venue) $further_where .= ' AND show_venue_id = ' . $wpdb->prepare('%d', $venue);
-	
+
 	// Date filtering
-	
+
 	// Query vars take precedence over function vars
-	if(isset($_REQUEST['gpy'])) { 
+	if(isset($_REQUEST['gpy'])) {
 		$year = $_REQUEST['gpy'];
-	
+
 		if(isset($_REQUEST['gpm'])) {
 			$month = $_REQUEST['gpm'];
 		} else {
-			unset($month);
+			$month = '';
 		}
 		$no_limit = TRUE;
 	}
-	
-	
+
+
 	// Validate year and date parameters
 	if($year || $month) {
-	
+
 		if($year) {
 			if(is_numeric($year) && strlen($year) == 4) {
 				$year = round($year);
@@ -91,7 +91,7 @@ function gigpress_shows($filter = null, $content = null) {
 			// We've only specified a month, so we'll assume the year is current
 			$year = date('Y', current_time('timestamp'));
 		}
-		
+
 		if($month) {
 			if($month == 'current') {
 				$month = date('m', current_time('timestamp'));
@@ -111,27 +111,27 @@ function gigpress_shows($filter = null, $content = null) {
 			}
 			$start_month = $end_month = $month;
 		}
-		
+
 		if(!$month) {
 			$start_month = '01';
 			$end_month = '12';
 		}
-		
+
 		$start = $year.'-'.$start_month.'-01';
 		$end = $year.'-'.$end_month.'-31';
 		$further_where .= ' AND show_date BETWEEN '.$wpdb->prepare('%s', $start).' AND '.$wpdb->prepare('%s', $end);
 	}
 
-	
+
 	$limit = ($limit && !isset($no_limit)) ? ' LIMIT ' . $wpdb->prepare('%d', $limit) : '';
 	$artist_order = ($artist_order == 'custom') ?  "artist_order ASC," : '';
-	
+
 	// With the new 'all' scope, we should probably have a third message option, but I'm too lazy
 	// Really, there should just be one generic 'no shows' message. Oh well.
 	$no_results_message = ($scope == 'upcoming') ? wptexturize($gpo['noupcoming']) : wptexturize($gpo['nopast']);
-	
+
 	ob_start();
-	
+
 	// Are we showing our menu?
 	if($show_menu) {
 		$menu_options = array();
@@ -148,22 +148,22 @@ function gigpress_shows($filter = null, $content = null) {
 		echo gigpress_menu($menu_options);
 		include gigpress_template('after-menu');
 	}
-	
+
 	$shows_markup = array();
-	
+
 	// If we're grouping by artist, we'll unfortunately have to first get all artists
 	// Then  make a query for each one. Looking for a better way to do this.
-	
-	if($group_artists == 'yes' && !$artist && $total_artists > 1) { 
-		
+
+	if($group_artists == 'yes' && !$artist && $total_artists > 1) {
+
 		$artists = $wpdb->get_results("SELECT * FROM " . GIGPRESS_ARTISTS . " ORDER BY " . $artist_order . "artist_alpha ASC");
-		
+
 		foreach($artists as $artist_group) {
 			$shows = $wpdb->get_results("SELECT * FROM " . GIGPRESS_ARTISTS . " AS a, " . GIGPRESS_VENUES . " as v, " . GIGPRESS_SHOWS ." AS s LEFT JOIN  " . GIGPRESS_TOURS . " AS t ON s.show_tour_id = t.tour_id WHERE " . $date_condition . " AND show_status != 'deleted' AND s.show_artist_id = " . $artist_group->artist_id . " AND s.show_artist_id = a.artist_id AND s.show_venue_id = v.venue_id " . $further_where . " ORDER BY s.show_date " . $sort . ",s.show_expire " . $sort . ",s.show_time ". $sort . $limit);
-			
+
 			if($shows) {
 				// For each artist group
-				
+
 				$some_results = TRUE;
 				$current_tour = '';
 				$i = 0;
@@ -173,21 +173,21 @@ function gigpress_shows($filter = null, $content = null) {
 					'artist_id' => $artist_group->artist_id,
 					'artist_url' => (!empty($artist_group->artist_url)) ? esc_url($artist_group->artist_url) : '',
 				);
-			
+
 				include gigpress_template('shows-artist-heading');
 				include gigpress_template('shows-list-start');
-											
+
 				foreach($shows as $show) {
-				
+
 					// For each individual show
-					
+
 					$showdata = gigpress_prepare($show, 'public');
-					
+
 					if($showdata['tour'] && $showdata['tour'] != $current_tour && !$tour) {
 						$current_tour = $showdata['tour'];
 						include gigpress_template('shows-tour-heading');
 					}
-					
+
 					$class = $showdata['status'];
 					++ $i; $class .= ($i % 2) ? '' : ' gigpress-alt';
 					if(!$showdata['tour'] && $current_tour) {
@@ -195,22 +195,22 @@ function gigpress_shows($filter = null, $content = null) {
 						$class .= ' gigpress-divider';
 					}
 					$class .= ($showdata['tour'] && !$tour) ? ' gigpress-tour' : '';
-					
+
 					include gigpress_template('shows-list');
-					
+
 					if($gpo['output_schema_json'] == 'y')
 					{
 						$show_markup = gigpress_json_ld($showdata);
 						array_push($shows_markup,$show_markup);
 					}
 				}
-				
-				include gigpress_template('shows-list-end');						
+
+				include gigpress_template('shows-list-end');
 			}
 		}
-		
+
 		if($some_results) {
-			// After all artist groups		
+			// After all artist groups
 			include gigpress_template('shows-list-footer');
 			if(!empty($shows_markup))
 			{
@@ -225,36 +225,36 @@ function gigpress_shows($filter = null, $content = null) {
 					echo json_encode($shows_markup, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 				}
 				echo '</script>';
-			}			
-		} else {	
+			}
+		} else {
 			// No shows from any artist
 			include gigpress_template('shows-list-empty');
 		}
-		
+
 	} else {
 
 		// Not grouping by artists
 
 		$shows = $wpdb->get_results("
 			SELECT * FROM " . GIGPRESS_ARTISTS . " AS a, " . GIGPRESS_VENUES . " as v, " . GIGPRESS_SHOWS ." AS s LEFT JOIN  " . GIGPRESS_TOURS . " AS t ON s.show_tour_id = t.tour_id WHERE " . $date_condition . " AND show_status != 'deleted' AND s.show_artist_id = a.artist_id AND s.show_venue_id = v.venue_id " . $further_where . " ORDER BY s.show_date " . $sort . ",s.show_expire " . $sort . ",s.show_time " . $sort . $limit);
-				
+
 		if($shows) {
-		
+
 			$current_tour = '';
 			$i = 0;
 
 			include gigpress_template('shows-list-start');
-			
+
 			foreach($shows as $show) {
-			
+
 				// For each individual show
 				$showdata = gigpress_prepare($show, 'public');
-				
+
 				if($showdata['tour'] && $showdata['tour'] != $current_tour && !$tour) {
 					$current_tour = $showdata['tour'];
 					include gigpress_template('shows-tour-heading');
 				}
-				
+
 				$class = $showdata['status'];
 				++ $i; $class .= ($i % 2) ? '' : ' gigpress-alt';
 				if(!$showdata['tour'] && $current_tour) {
@@ -262,16 +262,16 @@ function gigpress_shows($filter = null, $content = null) {
 					$class .= ' gigpress-divider';
 				}
 				$class .= ($showdata['tour'] && !$tour) ? ' gigpress-tour' : '';
-				
+
 				include gigpress_template('shows-list');
-				
+
 				if($gpo['output_schema_json'] == 'y')
 				{
 					$show_markup = gigpress_json_ld($showdata);
 					array_push($shows_markup,$show_markup);
 				}
 			}
-			
+
 			include gigpress_template('shows-list-end');
 			include gigpress_template('shows-list-footer');
 
@@ -289,23 +289,23 @@ function gigpress_shows($filter = null, $content = null) {
 				}
 				echo '</script>';
 			}
-			
+
 		} else {
 			// No shows to display
 			include gigpress_template('shows-list-empty');
-		}	
+		}
 
 	}
-	
+
 	echo('<!-- Generated by GigPress ' . GIGPRESS_VERSION . ' -->
 	');
-	return ob_get_clean();	
+	return ob_get_clean();
 }
 
 
 function gigpress_menu($options = null) {
-	
-	global $wpdb, $wp_locale, $gpo;	
+
+	global $wpdb, $wp_locale, $gpo;
 
 	extract(shortcode_atts(array(
 		'type' => 'monthly',
@@ -319,9 +319,9 @@ function gigpress_menu($options = null) {
 		'venue' => FALSE,
 		'sort' => 'desc'
 	), $options));
-	
+
 	$base .= (strpos($base, '?') === FALSE) ? '?' : '&amp;';
-		
+
 	// Date conditionals based on scope
 	switch($scope) {
 		case 'upcoming':
@@ -333,14 +333,14 @@ function gigpress_menu($options = null) {
 		case 'all':
 			$date_condition = "!= ''";
 	}
-	
+
 	$further_where = '';
-	
+
 	// Artist, tour and venue filtering
 	if($artist) $further_where .= ' AND show_artist_id = ' . $wpdb->prepare('%d', $artist);
 	if($tour) $further_where .= ' AND show_tour_id = ' . $wpdb->prepare('%d', $tour);
-	if($venue) $further_where .= ' AND show_venue_id = ' . $wpdb->prepare('%d', $venue);	
-	
+	if($venue) $further_where .= ' AND show_venue_id = ' . $wpdb->prepare('%d', $venue);
+
 	// Variable operajigamarations based on monthly vs. yearly
 	switch($type) {
 		case 'monthly':
@@ -354,33 +354,33 @@ function gigpress_menu($options = null) {
 			$title = ($title) ? wptexturize(strip_tags($title)) : __('Select Year', 'gigpress');
 			$current = (isset($_REQUEST['gpy'])) ? $_REQUEST['gpy'] : '';
 	}
-	
+
 	// Build query
 	$dates = $wpdb->get_results("
-		SELECT YEAR(show_date) AS year, " . $sql_select_extra . " count(show_id) as shows 
-		FROM ".GIGPRESS_SHOWS." 
-		WHERE show_status != 'deleted' 
-		AND show_date " . $date_condition . $further_where . " 
-		GROUP BY YEAR(show_date)" . $sql_group_extra . " 
+		SELECT YEAR(show_date) AS year, " . $sql_select_extra . " count(show_id) as shows
+		FROM ".GIGPRESS_SHOWS."
+		WHERE show_status != 'deleted'
+		AND show_date " . $date_condition . $further_where . "
+		GROUP BY YEAR(show_date)" . $sql_group_extra . "
 		ORDER BY show_date " . $sort);
-	
+
 	ob_start();
-	
+
 	if($dates) : ?>
-			
+
 			<select name="gigpress_menu" class="gigpress_menu" id="<?php echo $id; ?>">
 				<option value="<?php echo $base; ?>"><?php echo $title; ?></option>
 			<?php foreach($dates as $date) : ?>
 				<?php $this_date = ($type == 'monthly') ? $date->year.$date->month : $date->year; ?>
 				<option value="<?php echo $base.'gpy='.$date->year; if($type == 'monthly') echo '&amp;gpm='.$date->month; ?>"<?php if($this_date == $current) : ?> selected="selected"<?php endif; ?>>
-					<?php if($type == 'monthly') echo $wp_locale->get_month($date->month).' '; echo $date->year; ?> 
+					<?php if($type == 'monthly') echo $wp_locale->get_month($date->month).' '; echo $date->year; ?>
 					<?php if($show_count && $show_count == 'yes') : ?>(<?php echo $date->shows; ?>)<?php endif; ?>
 				</option>
 			<?php endforeach; ?>
 			</select>
-	
+
 	<?php endif;
-	
+
 	return ob_get_clean();
 }
 
@@ -395,29 +395,29 @@ function gigpress_has_upcoming($filter = null)
 			'venue' => FALSE
 		), $filter)
 	);
-	
+
 	// Artist, tour and venue filtering
 	if($artist) $further_where .= ' AND show_artist_id = ' . $wpdb->prepare('%d', $artist);
 	if($tour) $further_where .= ' AND show_tour_id = ' . $wpdb->prepare('%d', $tour);
 	if($venue) $further_where .= ' AND show_venue_id = ' . $wpdb->prepare('%d', $venue);
 
 	$shows = $wpdb->get_results("
-			SELECT show_id 
-			FROM " . GIGPRESS_SHOWS ." 
-			WHERE show_expire >= '" . GIGPRESS_NOW . "' 
-			AND show_status != 'deleted'" . $further_where . " 
+			SELECT show_id
+			FROM " . GIGPRESS_SHOWS ."
+			WHERE show_expire >= '" . GIGPRESS_NOW . "'
+			AND show_status != 'deleted'" . $further_where . "
 			LIMIT 1
 		");
 	if($shows) return true;
-	
-	
+
+
 }
 
 function gigpress_json_ld($showdata)
 {
 	// Start array for single event
 	$show_markup = array("@context" => "http://schema.org", "@type" => "Event");
-	
+
 	// Add show level attributes
 	$show_markup['name'] = (!empty($showdata['tour'])) ? $showdata['tour'] : $showdata['artist_plain'];
 	$show_markup['startDate'] = $showdata['iso_date'];
@@ -436,17 +436,17 @@ function gigpress_json_ld($showdata)
 
 	// Create performer
 	$performer_markup = array("@type" => "Organization");
-	
+
 	// Add performer attributes
 	$performer_markup['name'] = $showdata['artist_plain'];
 	if(!empty($showdata['artist_url'])) { $performer_markup['url'] = $showdata['artist_url']; }
-	
+
 	// Merge performer into show
 	$show_markup['performers'] = $performer_markup;
 
 	// Create venue
 	$location_markup = array("@type" => "Place");
-	
+
 	//Add venue attributes
 	$location_markup['name'] = $showdata['venue_plain'];
 	if(!empty($showdata['venue_url'])) { $location_markup['url'] = $showdata['venue_url']; }
@@ -454,7 +454,7 @@ function gigpress_json_ld($showdata)
 
 	// Create venue address
 	$address_markup = array("@type" => "PostalAddress");
-	
+
 	//Add address attributes
 	if(!empty($showdata['address_plain'])) { $address_markup['streetAddress'] = $showdata['address_plain']; }
 	$address_markup['addressLocality'] = $showdata['city'];
@@ -481,6 +481,6 @@ function gigpress_json_ld($showdata)
 	if(count($offer_markup) > 1) {
 		$show_markup['offers'] = $offer_markup;
 	}
-	
+
 	return $show_markup;
 }

1e88d01d526f674f576354bd4b092fac1739a63a
Scott Taylor <scott.c.taylor@mac.com>
2015-10-27 15:40:58 -0400

    Update footer.php

--- wp-content/themes/gp/footer.php
+++ wp-content/themes/gp/footer.php
@@ -1,7 +1,7 @@
 		</div>
 	</div>
 	<div class="footer">
-		<p>&copy; <?php	 echo date( 'Y' ) ?> Goodbye Picasso, New York, NY - All Rights Reserved.
+		<p>&copy; <?php	 echo date( 'Y' ) ?> Goodbye Picasso, New York, NY - All Rights Reserved. Photography by <a target="_blank" href="http://nickfallon.tumblr.com/">Nick Fallon</a>.
 		<a href="<?php echo home_url( '/credits/' ) ?>">Credits</a>.
 		</p>
 		<div id="facebook_widget">

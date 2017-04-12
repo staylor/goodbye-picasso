e7b78bc5bdb79cc1d6e230f5cd96599d1deb22bb
Scott Taylor <scott.c.taylor@mac.com>
2017-04-12 18:16:40 -0400

    new site

--- wp-content/themes/gp/index.php
+++ wp-content/themes/gp/index.php
@@ -1,8 +1,53 @@
-<?php
-get_header();
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Goodbye Picasso - New York, NY</title>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-//gp_lyrics( 'This neighborhood was filled with words, now everyone here speaks in numbers So I just reply in notes and measures' );
+    ga('create', 'UA-1544578-3', 'goodbyepicasso.com');
+    ga('send', 'pageview');
 
-get_template_part( 'loop' );
+  </script>
+</head>
+<body>
+<style>
+main {
+  background: #4a4a4a;
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
 
-get_footer();
+main img {
+  height: auto;
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+  -webkit-image-rendering: crisp-edges;
+  -moz-image-rendering: crisp-edges;
+  image-rendering: crisp-edges;
+}
+
+main iframe {
+  top: 2.5%;
+  left: 2.5%;
+  right: 2.5%;
+  position: fixed;
+  z-index: 20;
+}
+</style>
+<main>
+  <img src="/wp-content/themes/gp/images/single.jpg"/>
+  <iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A4wU443mzUgSfiLcF0dKV6f" width="95%" height="80" frameborder="0" allowtransparency="true"></iframe>
+</main>
+</body>
+</html>
